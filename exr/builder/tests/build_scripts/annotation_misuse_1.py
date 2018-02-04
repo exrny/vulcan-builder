15018cba15d8b73b040b3992c086a9fa58f8a8e5
Peter Salnikov <peter@salnikov.cc>
2018-02-05 00:28:06 +0300

    initial commit

---
+++ exr/builder/tests/build_scripts/annotation_misuse_1.py
@@ -0,0 +1,8 @@
+from exr.builder import task
+
+# Uses @_exrb.task form instead of @_exrb.task() form.
+
+
+@task
+def clean():
+    pass
---
+++ exr/builder/tests/build_scripts/annotation_misuse_3.py
@@ -0,0 +1,13 @@
+from exr.builder import task
+
+
+@task()
+def clean():
+    pass
+
+# Referring to clean by name rather than reference.
+
+
+@task(1234)
+def html():
+    pass
